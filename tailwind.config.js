1a3bdff127b4dccfdefd16f7e9ad0f50252a4b27
Yulia Shyshka <shyshka20@gmail.com>
2022-11-16 17:48:46 +0200

    fix tailwand

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,15 +18,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-// export const content = [
-//   "./app/**/*.{js,ts,jsx,tsx}",
-//   "./pages/**/*.{js,ts,jsx,tsx}",
-//   "./components/**/*.{js,ts,jsx,tsx}",
-// ];
-// export const theme = {
-//   extend: {},
-// };
-// export const plugins = [
-//   require('@tailwindcss/forms'),
-// ];
