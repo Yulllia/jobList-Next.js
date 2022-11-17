987a832b22a33482fc3048d4970a364754185975
Yulia Shyshka <shyshka20@gmail.com>
2022-11-17 20:49:20 +0200

    add changes

--- components/JobList/JobList.tsx
+++ components/JobList/JobList.tsx
@@ -14,7 +14,7 @@ function JobList() {
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token="wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"`
+      `https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
     )
       .then((res) => res.json())
       .then((data) => {
--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'custom',
-    loaderFile: './loader.tsx',
+    unoptimized: true,
   },
   experimental: {
     forceSwcTransforms: false,
